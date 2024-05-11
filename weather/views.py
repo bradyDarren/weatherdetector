299221a33ce4ab4e884bd3cc349a2d3864dd5855
Darren Brady <dcgbfinancial@gmail.com>
2024-05-11 13:52:40 -0500

    Removal of errors within code.

--- weather/views.py
+++ weather/views.py
@@ -7,8 +7,8 @@ import urllib.request
 def index(request):
     if request.method == "POST":
         city = request.POST['CityInput']
-        res = urllib,request.urlopn('http://api.openweathermap.org/data/2.5/weather?q'+city+'&appid=16fb8560f0c501abec59b9b2768be345').read()
-        json_data = json.load(res)
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=16fb8560f0c501abec59b9b2768be345').read()
+        json_data = json.loads(res)
         data = {
             "counrty_code": str(json_data['sys']['country']),
             "coordinate": str(json_data['coord']['lon']) + ' '+ 
