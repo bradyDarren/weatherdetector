1d7e8adce11338668c06c1703161f1863e34230a
Darren Brady <dcgbfinancial@gmail.com>
2024-05-11 13:32:00 -0500

    Addition of the openweather API to make sure that when the city is input into the search bar the appropriate information populates.

--- weather/views.py
+++ weather/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+import json 
+import urllib.request
 
 # Create your views here.
 
 def index(request):
     if request.method == "POST":
         city = request.POST['CityInput']
+        res = urllib,request.urlopn('http://api.openweathermap.org/data/2.5/weather?q'+city+'&appid=16fb8560f0c501abec59b9b2768be345').read()
+        json_data = json.load(res)
+        data = {
+            "counrty_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' '+ 
+            str(json_data['coord']['lat']),
+            'temp': str(json_data['main']['temp'])+'k',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
     else:
-        city = ""
-    return render(request, 'index.html',{'city': city})
+        data = {}
+    return render(request, 'index.html', data)
